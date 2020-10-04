0bfbe1148a24c7603e388da0a71206a1f94742c7
Westerbly Snaydley <westerbly@gmail.com>
2020-10-04 10:58:08 -0500

    add nfd to build script

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@
 set -euo pipefail
 
 gcc -shared -o ./bin/libsokol.so sokol.c -fPIC -pthread -lX11 -lXi -lXcursor -lGL -ldl -lm
+make -C ./nativefiledialog/build/gmake_linux/ clean
+make -C ./nativefiledialog/build/gmake_linux/ CFLAGS=-fPIC
 gcc -o ./bin/libnfd.so -shared -Wl,--whole-archive ./nativefiledialog/build/lib/Release/x64/libnfd.a -Wl,--no-whole-archive
