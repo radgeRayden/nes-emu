efb334e6e7b257144b91f54c52af45c37e4318c3
Westerbly Snaydley <westerbly@gmail.com>
2021-04-13 23:51:24 -0500

    small fix to sokol / cimgui build on windows

--- native/build_windows.sh
+++ native/build_windows.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-gcc -shared -o ./bin/sokol.dll sokol.c -fPIC -lkernel32 -lgdi32 -luser32 -pthread -lstdc++ -L./cimgui -lcimgui -Wl,--export-all
+gcc -shared -o ./bin/sokol.dll sokol.c -fPIC -lkernel32 -lgdi32 -luser32 -pthread -L./cimgui -lcimgui -Wl,--export-all -lstdc++
 make -C ./nativefiledialog/build/gmake_windows/ clean
 make -C ./nativefiledialog/build/gmake_windows/ CFLAGS=-fPIC
 gcc -o ./bin/nfd.dll -shared ./nativefiledialog/build/obj/x64/Release/nfd/nfd_common.o ./nativefiledialog/build/obj/x64/Release/nfd/nfd_win.o -lole32 -luuid
